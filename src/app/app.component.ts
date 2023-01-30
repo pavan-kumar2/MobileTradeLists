3a101e9d55d58ff3deeb80addfeb6195c58500cd
pavan-kumar2 <pavankkarkera480@gmail.com>
2023-01-30 12:29:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  HttpClient
+} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mobile-trade';
+  years: any = null;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/2018')
+      .subscribe(res => {
+        console.log(res);
+        this.years = res;
+      })
+  }
+
+
 }
